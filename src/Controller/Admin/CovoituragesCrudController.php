945ece5138491abb9443ea0fff5b3237b6c39db7
codealex1 <alex2005.brunet@gmail.com>
2025-02-04 14:28:02 +0100

    prod0

--- src/Controller/Admin/CovoituragesCrudController.php
+++ src/Controller/Admin/CovoituragesCrudController.php
@@ -4,12 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Covoiturages;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 
 class CovoituragesCrudController extends AbstractCrudController
 {
@@ -34,7 +35,8 @@ class CovoituragesCrudController extends AbstractCrudController
             TextField::new('lieu_arrivee', 'Lieu d\'arrivée'),
             NumberField::new('nb_place', 'Nombre de places'),
             NumberField::new('prix_personne', 'Prix par personne'),
-            
+            BooleanField::new('isActive', 'Actif'), 
+            BooleanField::new('isStarted', 'Démarré'),
         ];
     }
     
